5fe2e6af168658699443dc59d6c2287fc99c33fd
Osvaldo Antunes <meloosvaldo@hotmail.com>
2022-08-20 18:44:06 -0300

    primeiro teste do tutorial

---
+++ Documents/NetBeansProjects/Apptutorial/src/apptutorial/Apptutorial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package apptutorial;
+
+/**
+ *
+ * @author user
+ */
+public class Apptutorial {
+    public static void main(String[] args) {
+        System.out.println("Ola, mundo!");
+    }
+    
+}
